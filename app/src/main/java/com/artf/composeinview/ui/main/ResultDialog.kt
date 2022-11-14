cc848aa10e3a51df8e29423c893104ba58368071
QArtur99 <artur2113@o2.pl>
2022-11-15 00:35:20 +0100

    ComposeView directly as layout

--- app/src/main/java/com/artf/composeinview/ui/main/ResultDialog.kt
+++ app/src/main/java/com/artf/composeinview/ui/main/ResultDialog.kt
@@ -1,13 +1,25 @@
 package com.artf.composeinview.ui.main
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
+import com.artf.composeinview.R
 import com.artf.composeinview.databinding.DialogResultBinding
+import android.app.AlertDialog as AlertDialogWidget
 
 class ResultDialog : DialogFragment() {
 
@@ -33,7 +45,48 @@ class ResultDialog : DialogFragment() {
         binding.buttonOk.setOnClickListener {
             dismiss()
         }
-        val dialog = AlertDialog.Builder(requireContext()).setView(binding.root).create()
+
+        val view = ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    val state = viewModel.msg.observeAsState()
+                    AlertDialog(
+                        onDismissRequest = { dismiss() },
+                        modifier = Modifier.fillMaxWidth(),
+                        title = {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center,
+                                text = state.value ?: ""
+                            )
+                        },
+                        buttons = {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                Button(onClick = {
+                                    viewModel.setReset(true)
+                                    dismiss()
+                                }) {
+                                    Text(text = getString(R.string.reset))
+                                }
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Button(onClick = { dismiss() }) {
+                                    Text(text = getString(R.string.ok))
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+
+
+        val dialog = AlertDialogWidget.Builder(requireContext()).setView(view).create()
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return dialog
     }
